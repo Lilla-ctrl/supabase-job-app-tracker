908b95be5b5132b7237f794fe88b878ddede0431
Lilla-ctrl <berenyi.lilu@gmail.com>
2026-01-08 15:25:04 +0100

    Initial Supabase setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,106 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { supabase } from "./helpers/supabase-client";
 
 function App() {
-  return (<div>hello</div>
-  )
+  const [newJob, setNewJob] = useState({
+    company: "",
+    position: "",
+    status: "",
+  });
+  const [jobs, setJobs] = useState([]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const { error } = await supabase.from("job_applications").insert(newJob);
+
+    if (error) {
+      console.error("Error adding job:", error.message);
+      return;
+    }
+
+    setNewJob("");
+  }
+
+  async function handleDelete(id) {
+    const { error } = await supabase
+      .from("job_applications")
+      .delete()
+      .eq("id", id);
+
+    if (error) {
+      console.error("Error deleting job:", error.message);
+      return;
+    }
+  }
+
+  async function fetchJobs() {
+    const { error, data } = await supabase
+      .from("job_applications")
+      .select("*")
+      .order("created_at", { ascending: true });
+
+    if (error) {
+      console.error("Error reading job:", error.message);
+      return;
+    }
+
+    setJobs(data);
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <form action="" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="company"
+            onChange={(e) =>
+              setNewJob((prev) => ({ ...prev, company: e.target.value }))
+            }
+          />
+          <input
+            type="text"
+            placeholder="position"
+            onChange={(e) =>
+              setNewJob((prev) => ({ ...prev, position: e.target.value }))
+            }
+          />
+          <input
+            type="text"
+            placeholder="status"
+            onChange={(e) =>
+              setNewJob((prev) => ({ ...prev, status: e.target.value }))
+            }
+          />
+          <button className="hover:cursor-pointer" type="submit">
+            submit
+          </button>
+        </form>
+      </div>
+      <div>Jobs:</div>
+      {jobs.map((job, key) => (
+        <div key={key}>
+          <div>
+            <h2>Company: {job.company}</h2>
+            <h3>Position: {job.position}</h3>
+            <h3>Status: {job.status}</h3>
+          </div>
+          <div>
+            <button>Edit</button>
+          </div>
+          <div>
+            <button onClick={() => handleDelete(job.id)}>Delete</button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default App
+export default App;

dd1a3f4dc8bb67f965358fafac7c3b59b041def8
Lilla-ctrl <berenyi.lilu@gmail.com>
2026-01-15 14:23:45 +0100

    update styling of Auth component

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -4,6 +4,7 @@ import { supabase } from "../helpers/supabase-client";
 export default function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [user, setUser] = useState("");
   const [isSigningUp, setIsSigningUp] = useState(true);
 
   async function handleSubmit(event) {
@@ -34,25 +35,67 @@ export default function Auth() {
 
   return (
     <>
-      <h2>{isSigningUp ? "Sign Up" : "Sign In"}</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">{isSigningUp ? "Sign Up" : "Sign In"}</button>
-        <button onClick={() => setIsSigningUp(!isSigningUp)}>
-          {isSigningUp ? "Already a member? Sign in!" : "Not a member yet? Sign up!"}
-        </button>
-      </form>
+      <div className="bg-gray-300 fixed inset-0 flex items-center justify-center min-h-screen ">
+        <div className="bg-gray-100 p-6 flex justify-center rounded-xl shadow-lg w-64">
+          <div>
+            <div className="text-xl mb-4">
+              <h2>{isSigningUp ? "Sign Up" : "Sign In"}</h2>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <div className="text-sm mb-1">Email address</div>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="text-sm border border-gray-300 shadow-sm p-1 mb-5 w-full focus:border-teal-400 focus:ring-teal-600"
+                />
+              </div>
+              <div>
+                <div className="text-sm mb-1">Password</div>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="border border-gray-300 shadow-sm p-1 mb-5 w-full focus:border-teal-400 focus:ring-teal-600"
+                />
+              </div>
+
+              {isSigningUp && (
+                <>
+                  <div className="text-sm mb-1">User name</div>
+                  <input
+                    type="text"
+                    value={user}
+                    onChange={(e) => setUser(e.target.value)}
+                    className="border border-gray-300 shadow-sm p-1 mb-5 w-full focus:border-teal-400 focus:ring-teal-600"
+                  />
+                </>
+              )}
+
+              <div>
+                <button
+                  type="submit"
+                  className="bg-teal-500 text-white text-sm font-medium py-2 px-3 rounded-full w-full hover:bg-teal-600 cursor-pointer"
+                >
+                  {isSigningUp ? "Sign Up" : "Sign In"}
+                </button>
+              </div>
+              <div className="flex justify-center">
+                <div className="text-sm mb-3">
+                  {isSigningUp ? "Already a member?" : "Not a member yet?"}
+                  <button
+                    onClick={() => setIsSigningUp(!isSigningUp)}
+                    className="cursor-pointer text-teal-500 ml-1"
+                  >
+                    {isSigningUp ? "Sign in!" : "Sign up!"}
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 }

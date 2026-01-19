54deed3048dd8161ae4bbcdf16e15b5f724f328a
Lilla-ctrl <berenyi.lilu@gmail.com>
2026-01-19 14:56:01 +0100

    update Modal styling

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,7 +9,7 @@ export default function Modal({
 }) {
   return (
     <div className="bg-black/60 fixed inset-0 flex items-center justify-center min-h-screen ">
-      <div className="bg-amber-50 p-6 rounded shadow-lg max-w-lg md:max-w-xl w-full ">
+      <div className="bg-primary text-text p-6 rounded shadow-lg max-w-lg md:max-w-xl w-full ">
         <form
           onSubmit={(e) => {
             e.preventDefault();
@@ -22,7 +22,7 @@ export default function Modal({
         >
           <div>
             <div className="space-y-4">
-              <label htmlFor="company" className="text-gray-500 text-lg">
+              <label htmlFor="company" className="text-text text-lg">
                 Company
               </label>
               <input
@@ -34,12 +34,12 @@ export default function Modal({
                 name="company"
                 id="company"
                 required
-                className="shadow-inner p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-amber-400
+                className="shadow-inner p-2 border border-jobcard-border rounded-md w-full focus:outline-none focus:ring-2 focus:ring-modal-ring
 "
               />
             </div>
             <div className="space-y-4">
-              <label htmlFor="position" className="text-gray-500 text-lg">
+              <label htmlFor="position" className="text-text text-lg">
                 Position
               </label>
               <input
@@ -51,12 +51,12 @@ export default function Modal({
                 name="position"
                 id="position"
                 required
-                className="shadow-inner p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-amber-400
+                className="shadow-inner p-2 border border-jobcard-border rounded-md w-full focus:outline-none focus:ring-2 focus:ring-modal-ring
 "
               />
             </div>
             <div className="space-y-4">
-              <label htmlFor="contact" className="text-gray-500 text-lg">
+              <label htmlFor="contact" className="text-text text-lg">
                 Contact info
               </label>
               <input
@@ -67,11 +67,11 @@ export default function Modal({
                 type="text"
                 name="contact"
                 id="contact"
-                className="shadow-inner p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-2 focus:ring-amber-400
+                className="shadow-inner p-2 border border-jobcard-border rounded-md w-full focus:outline-none focus:ring-2 focus:ring-modal-ring
 "
               />
             </div>
-            <label htmlFor="date" className="text-gray-500 text-lg mr-2">
+            <label htmlFor="date" className="text-text text-lg mr-2">
               Date of application
             </label>
             <input
@@ -92,7 +92,7 @@ export default function Modal({
               id="notes"
               rows="4"
               placeholder=" Add any notes here"
-              className="border mt-4 mb-2 w-full rounded-md border-gray-300 shadow-sm focus:border-teal-600 focus:ring-teal-600 sm:text-md"
+              className="border mt-4 mb-2 w-full rounded-md border-jobcard-border shadow-sm focus:outline-none focus:ring-2 focus:ring-modal-ring sm:text-md"
             ></textarea>
             <select
               value={selectedJob?.status || ""}
@@ -101,7 +101,7 @@ export default function Modal({
               }
               name="status"
               id="status-select"
-              className="shadow-inner p-2 border border-gray-300 rounded-md w-full text-gray-600 focus:outline-none focus:ring-2 focus:ring-amber-400"
+              className="bg-primary shadow-inner p-2 border border-jobcard-border rounded-md w-full text-text focus:outline-none focus:ring-2 focus:ring-modal-ring"
             >
               <option value="">Application status</option>
               <option value="Applied">Applied</option>
@@ -114,7 +114,7 @@ export default function Modal({
 
           <button
             type="submit"
-            className="border border-gray-300 text-gray-500 rounded-md px-3 py-1 mt-5 hover:bg-gray-300 cursor-pointer"
+            className="border border-jobcard-border text-text rounded-md px-3 py-1 mt-5 hover:bg-modal-button-hover cursor-pointer"
           >
             Save
           </button>
@@ -124,7 +124,7 @@ export default function Modal({
               isOpen(false);
             }}
             type="button"
-            className="border border-gray-300 text-gray-500 rounded-md px-3 py-1 mt-5 hover:bg-gray-300 cursor-pointer ml-2"
+            className="border border-jobcard-border text-text rounded-md px-3 py-1 mt-5 hover:bg-modal-button-hover cursor-pointer ml-2"
           >
             Cancel
           </button>

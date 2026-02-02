ab5bed12cbce0cdc47cda0fa4fda3c52371c3b1c
Lilla-ctrl <berenyi.lilu@gmail.com>
2026-02-02 15:01:10 +0100

    update Modal styling

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -277,7 +277,7 @@ export default function Modal({
                     setFormData((prev) => ({ ...prev, notes: e.target.value }))
                   }
                   id="notes"
-                  rows="4"
+                  rows="3"
                   placeholder="Add any notes here"
                   className="placeholder:text-sm placeholder:text-text/30 resize-none mb-0 shadow-inner bg-primary/50 px-4 py-3 border border-text/10 text-text rounded-xl w-full focus:outline-none focus:ring-2 focus:ring-text/20 outline-none transition-all"
                 ></textarea>

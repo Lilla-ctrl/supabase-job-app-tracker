bfbc295812f5acece85a7890f6dd02c606586d5b
Lilla-ctrl <berenyi.lilu@gmail.com>
2026-02-02 15:36:13 +0100

    update Modal styling

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -70,7 +70,7 @@ export default function Modal({
 
   return (
     <div
-      className={`fixed inset-0 flex items-center justify-center min-h-screen z-50 transition-all duration-500 ${
+      className={`fixed inset-0 flex items-center justify-center min-h-dvh z-50 transition-all duration-500 ${
         isOpen
           ? "opacity-100 backdrop-blur-sm pointer-events-auto"
           : "opacity-0 backdrop-blur-0 pointer-events-none"
@@ -81,7 +81,7 @@ export default function Modal({
         onClick={() => setIsOpen(false)}
       />
       <div
-        className={`relative mx-4 px-6 py-3 sm:px-12 sm:py-8 sm:mx-0 bg-secondary text-text sm:max-w-lg rounded-2xl shadow-2xl max-w-lg md:max-w-xl w-full border border-jobcard-border transition-all duration-500 ease-out-back rounded-t-4xl sm:rounded-2xl max-h-[90vh] flex flex-col
+        className={`relative mx-4 my-auto px-6 py-3 sm:px-12 sm:py-8 sm:mx-0 bg-secondary text-text sm:max-w-lg rounded-2xl shadow-2xl max-w-lg md:max-w-xl w-full border border-jobcard-border transition-all duration-500 ease-out-back rounded-t-4xl sm:rounded-2xl max-h-[90vh] flex flex-col
             ${
               isOpen
                 ? "translate-y-0 scale-100 opacity-100 blur-none"
